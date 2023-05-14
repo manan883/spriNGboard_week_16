97f8bbf6a9ef07dc774a6f44b9f10c8e60795727
manan <manan@aws.com>
2023-05-14 14:46:17 -0700

    added fizzbuzz

--- exercises/exercise_04_pytest/fizzbuzz/test_fizzbuzz.py
+++ exercises/exercise_04_pytest/fizzbuzz/test_fizzbuzz.py
@@ -1,8 +1,11 @@
 import pytest
 
 def fizzbuzz(N):
-    pass
+    if N%2 == 0:
+        return "fizz"
+    else:
+        return "buzz"
 
 def test_fizzbuzz():
-    pass
+    assert fizzbuzz(2) == "fizz"
 
